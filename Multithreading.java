8b13808c59e173f84424ef1f3d0b6a0a1f8a5ae8
abiruba54-lab <abiruba54@gmail.com>
2025-08-11 16:09:12 +0530

    Add files via upload

---
+++ Multithreading.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class RandomNumberThread extends Thread
+{ 
+Random num = new Random();
+int value;
+public void run()
+{
+while(true){
+try {
+this.sleep(1000);
+} 
+catch (InterruptedException e) {
+}
+value = num.nextInt(1000);
+System.out.println("RandomNumberThread generated a number "+value);
+if(value % 2 == 0)
+new SquareGenThread(value).start();
+else
+new CubeGenThread(value).start();
+}
+}
+}
+class SquareGenThread extends Thread
+{
+int number;
+int squre;
+public SquareGenThread(int number) {
+this.number = number;
+}
+public void run()
+{
+try {
+this.sleep(3000);
+} 
+catch (InterruptedException e) {}
+this.squre = this.number * this.number; 
+System.out.println("SquareGenThread--> Square of "+number+" is "+squre);
+}
+}
+class CubeGenThread extends Thread
+{
+int number; 
+int squre;
+public CubeGenThread(int number) {
+this.number = number;
+}
+public void run()
+{
+try {
+this.sleep(2000);
+} catch (InterruptedException e) {}
+this.squre = this.number * this.number * this.number; 
+System.out.println("CubeGenThread--> Square of "+number+" is "+squre);
+}
+}
+class multithread
+{
+public static void main(String[] args) 
+{
+new RandomNumberThread().start();
+}
+}
